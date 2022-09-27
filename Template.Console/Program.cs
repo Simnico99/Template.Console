3b344bfff978017b06a1922897877dffde66be80
Simnico99 <Simnico99@hotmail.com>
2022-09-27 15:31:35 -0400

    Initial Upload.

---
+++ Template.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using TaskForceUtilities.Startup;
+using ZirconNet.WPF.DependencyInjection;
+
+try
+{
+    //Create
+    var host = new HostBuilder();
+    var config = new ConfigurationBuilder().RegisterConfigurations().Build();
+
+    //Configure
+    host.ConfigureAppConfiguration(x => x.AddConfiguration(config));
+    host.ConfigureServices(x => x.RegisterServices());
+    host.ConfigureLoggers(config);
+
+    //Use
+    host.UseSerilog();
+    host.UseBackgroundServices();
+    host.UseConsoleLifetime();
+
+    //Start
+    await host.StartAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Software crashed: {Exception}");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+    Environment.Exit(0);
+}
---
+++ Template.Console/Startup/RegisterConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskForceUtilities.Startup
+{
+    public static partial class IConfigurationBuilderExtension
+    {
+        public static IConfigurationBuilder RegisterConfigurations(this IConfigurationBuilder configuration)
+        {
+            configuration.SetBasePath(Directory.GetCurrentDirectory());
+            configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            configuration.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("DOTNET_") ?? "Production"}.json", optional: true);
+            configuration.AddEnvironmentVariables();
+            configuration.Build();
+
+            return configuration;
+        }
+    }
+}
